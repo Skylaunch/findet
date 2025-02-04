55b90ea28eb19110e2000403206e805a77ffbc7e
Maxim Grigorev <m.grigorev@webregul.ru>
2025-02-04 20:10:43 +0300

    Небольшие правки темизации

--- lib/blocs/global/theme_bloc.dart
+++ lib/blocs/global/theme_bloc.dart
@@ -1,6 +1,3 @@
-import 'package:findet/assets/themes/colors.dart';
-import 'package:findet/assets/themes/dark_colors.dart';
-import 'package:findet/assets/themes/white_colors.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 /// Эвент блока темизации
@@ -12,26 +9,18 @@ class ThemeChangedEvent extends ThemeEvent {}
 /// Стейт блока темизации
 abstract class ThemeState {
   bool get isDark;
-
-  ColorStyle get colorStyle;
 }
 
 /// Стейт тёмной темы
 class DarkThemeState extends ThemeState {
   @override
   bool get isDark => true;
-
-  @override
-  ColorStyle get colorStyle => DarkColorStyle();
 }
 
 /// Стейт светлой темы
 class WhiteThemeState extends ThemeState {
   @override
   bool get isDark => false;
-
-  @override
-  ColorStyle get colorStyle => WhiteColorStyle();
 }
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
